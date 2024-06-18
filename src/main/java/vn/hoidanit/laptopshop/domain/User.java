43a75dd4b76da51bac73f5298d735ff21f59ad50
kira <thanh0919234514@gmail.com>
2024-06-18 14:54:05 +0700

    views-product

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -10,6 +10,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "users")
@@ -17,10 +20,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
+    @NotNull
+    @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[azA-Z0-9.-]+$")
     private String email;
-
+    @NotNull
+    @Min(value = 10, message = "Password must have min 10 ")
     private String password;
+    @NotNull
+    @Min(value = 10, message = "FullName must have min 10 ")
     private String fullName;
     private String address;
     private String phone;
